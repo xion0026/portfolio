8477de93a031d9ad3363f9bdc2a74de1c8090363
Yingxin Xiong <xion0026@algonquinlive.com>
2026-04-14 16:47:15 -0400

    1

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
+
+const HeroTitle = () => {
+  return (
+    <h1 className="text-5xl font-bold leading-tight text-gray-900">
+      Where{' '}
+      {/* product empathy */}
+      <span className="relative cursor-default group transition-all duration-300 hover:text-purple-500">
+        <span className="invisible group-hover:visible absolute -left-6 opacity-50 font-mono text-sm">&lt;design&gt;</span>
+        product empathy
+        <span className="invisible group-hover:visible absolute -right-8 opacity-50 font-mono text-sm">&lt;/&gt;</span>
+      </span>
+      {' '}meets{' '}
+      {/* pixel-perfect code */}
+      <span className="relative cursor-default group transition-all duration-300 hover:text-emerald-500">
+        <span className="invisible group-hover:visible absolute -left-6 opacity-50 font-mono text-sm">&lt;code&gt;</span>
+        <span className="group-hover:font-mono transition-all">pixel-perfect code</span>
+        <span className="invisible group-hover:visible absolute -right-8 opacity-50 font-mono text-sm">&lt;/&gt;</span>
+      </span>
+      .
+    </h1>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeroTitle />);
